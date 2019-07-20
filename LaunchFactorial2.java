26cefd562dff9e79c53177b142f9f8f727fb9ce7
Govardhan <53010190+govardhankinkiri@users.noreply.github.com>
2019-07-20 22:22:25 +0530

    Add files via upload

---
+++ LaunchFactorial2.java
@@ -0,0 +1,29 @@
+package Day5;
+import java.util.*;
+class Factorial
+{
+	int number;
+	int calculateFactorial(int number)
+	{
+		this.number=number;
+		int product=1;
+		for(int i=1;i<=number;i++)
+		{
+			product=product*i;
+		}
+		return product;
+	}
+
+}
+class LaunchFactorial2
+{
+	public static void main(String[] args)
+	{
+		System.out.println("enter the number");
+		Scanner scan=new Scanner(System.in);
+		int number=scan.nextInt();
+		Factorial f= new Factorial();
+		System.out.println("Factorial  of "+number+" is "+f.calculateFactorial(number));
+		
+	}
+}
