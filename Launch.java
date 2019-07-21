580c9f581c62474fd16f25aa2b932ad5a25f3d7f
Govardhan <53010190+govardhankinkiri@users.noreply.github.com>
2019-07-21 17:00:21 +0530

    Add files via upload

---
+++ Launch.java
@@ -0,0 +1,19 @@
+package day01;
+ class Getmessage 
+{
+	void getmessage()
+	{
+		System.out.println("Hello world!");
+	}
+
+}
+ class Launch
+{
+	public static void main(String[] args )
+	{
+		Getmessage g = new Getmessage();
+		g.getmessage();
+		
+		
+	}
+}
