a09a9c7619f18f2929d4f20b86aa58c8636c5d1a
Govardhan <53010190+govardhankinkiri@users.noreply.github.com>
2019-08-21 21:59:16 +0530

    Add files via upload

---
+++ MarkValidatorUsingIfElse.java
@@ -0,0 +1,61 @@
+package day4;
+
+import java.util.Scanner;
+
+public class MarkValidatorUsingIfElse {
+	int marks;
+	public boolean isPass(int marks) {
+		
+		this.marks= marks;
+		if(marks>=40) 
+		{
+			return true;
+		}
+		else
+		{
+		
+			return false;
+		}
+	}
+	public char markGrade(int marks) {
+	
+			if(marks>90)
+			{
+				return 'A';
+			}
+			else if(marks>75){
+				return 'B';
+			}
+			else if(marks>60)
+			{
+				return 'C';
+			}
+			else 
+			{
+				return 'D';
+			}
+			
+		
+	}
+
+}
+class LaunchMarkValidator {
+	public static void main(String[] args) {
+		
+	    System.out.println("enter marks");
+		Scanner scan = new Scanner(System.in);
+		int marks = scan.nextInt();
+		MarkValidatorUsingIfElse m = new MarkValidatorUsingIfElse();
+	    String result = String.valueOf(m.isPass(marks));
+	    if(result == "true") 
+	    {
+		      System.out.println("Strudent Grade is :"+m.markGrade(marks));
+	    }
+	   else 
+	   {
+		      System.out.println("Student Fail");
+	   }
+	
+	
+   }
+}
