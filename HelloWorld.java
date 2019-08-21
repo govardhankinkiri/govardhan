5a8e5b71dc0002d29eb854eee5c719294242bdc9
Govardhan <53010190+govardhankinkiri@users.noreply.github.com>
2019-08-21 12:34:23 +0530

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,15 @@
+
+package day1;
+public class HelloWorld {
+	public String getMessage (){
+		return "Hello World!" ;
+	}
+	
+
+
+	public static void main(String[] args) {
+		HelloWorld h =new HelloWorld();
+		System.out.println(h.getMessage());
+		
+	}
+}
